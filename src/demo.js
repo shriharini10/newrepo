fdbe1b5e0f042443428e6ade390ec77ea640117b
shriharini10 <shri13anitha@gmail.com>
2024-05-27 00:28:43 +0530

    feat: compile javascript

---
+++ src/demo.js
@@ -0,0 +1,12 @@
+"use strict";
+const stud = {
+    name: "harini",
+    rollno: 2,
+    id: 3714,
+    printst() {
+        console.log('${this.name} ${this.rollno}');
+    }
+};
+console.log("Student information:");
+stud.printst();
+console.log(stud.id);
